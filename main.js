d5e38bacd11d7d79f20b7e2a2f664a1e49c0c78a
Bud_Ton <d2023ujle@gmail.com>
2024-11-20 00:25:38 +0300

    Updates

---
+++ main.js
@@ -0,0 +1,23 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/app.js
+// TODO: write code here
+
+// comment this to pass build
+const unusedVariable = "variable";
+
+// for demonstration purpose only
+function demo(value) {
+  return `Demo: ${value}`;
+}
+console.log(unusedVariable);
+console.log("app.js included");
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+// TODO: write your code in app.js
+/******/ })()
+;
